c98ee21bc938f0010367c9a854d2eabb590bbbff
Duniaalkilany <duniaalkilany908@gmail.com>
2021-08-23 20:55:24 +0300

    testing

--- caps.js
+++ caps.js
@@ -1,44 +1,49 @@
 'use strict';
 
-const events = require('./events.js');
-require('./driver/driver');
-require('./vendor/vendor');
-
-events.on('pickup', pickUp);
-events.on('in-transit', inTransit);
-events.on('delivered', delivered);
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+const io = require('socket.io')(port);
+const caps = io.of('/caps');
 
 
-function pickUp(payload) {
-    let result = {
-      event : 'pickup',
-      time: new Date().toLocaleString(),
-      payload : payload,
-    };
-    console.log('Event', result);
-  }
-
-  function inTransit(payload) {
-    let result = {
-      event : 'inTransit',
-      time: new Date().toLocaleString(),
-      payload : payload,
-    };
-    console.log('Event', result);
-  }
-
-  function delivered(payload) {
-    let result = {
-      event : 'delivered',
-      time: new Date().toLocaleString(),
-      payload : payload,
-    };
-    console.log('Event', result);
-  }
 
 
-  module.exports = {
-    pickUp,
-    inTransit,
-    delivered,
-  };
+// Global Operations - Default Namespace
+io.on('connection', (socket) => {
+  console.log("Welcome to Global Connection ! ", socket.id);
+
+});
+caps.on('connection', (socket) => {
+  console.log('caps: You are now connected to the CAPS system', socket.id);
+
+
+  socket.on('join', room => {
+    socket.join(room);
+  })
+  
+
+  socket.on('pickup', payload => {
+   let event = { event: 'pickup', time: new Date().toLocaleString(), payload: payload}
+    console.log('EVENT', event);
+    caps.emit('pickup', payload);
+  })
+
+
+  socket.on('in-transit', payload => {
+   let event = { event: 'in-transit', time: new Date().toLocaleString(), payload: payload}
+    console.log('EVENT', event);
+    caps.emit('in-transit', payload);
+  })
+  
+  socket.on('delivered', payload => {
+    let event = { event: 'delivered', time: new Date(), payload: payload}
+    console.log('EVENT', event)
+    caps.emit('delivered', payload);
+  })
+
+
+})
+
+module.exports=caps
+
+  
